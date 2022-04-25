9236820945ac8ff8c4ded1c510a6909fc0d5b95e
Hympert Nguyen <hnguyen013@alvamfg.local>
2022-04-25 16:49:17 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { items } from './items';
+
+const ItemCard = ({ item }) => {
+	return <div className='itemcard'>{item.name}</div>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='left'>
+				{Object.entries(items).map(([key, val]) => (
+					<ItemCard key={key} item={val} />
+				))}
+			</div>
+			<div className='right'>
+				<h3>Cart</h3>
+				<ul>
+					<li>
+						Milk tea <span>x1</span>
+						<span>$5.00</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
