d3681531d192f926e9c9a7921b8c8ce9826ba227
Hympert Nguyen <fieridneil@gmail.com>
2022-04-27 00:59:13 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import { categories, items, addons } from './items';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { categories, items, addons } from "./items";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import boba from "./assets/boba.PNG";
 
 const ItemCard = ({ item }) => {
 	return (
-		<div className='itemcard'>
-			<div className='item-image'>image</div>
-			<div className='item-info'>
+		<div className="itemcard">
+			<div className="item-image">image</div>
+			<div className="item-info">
 				<div>{item.name}</div>
 				<div>{`$${item.price.toFixed(2)}`}</div>
 				<div>{item.description}</div>
@@ -39,7 +40,7 @@ const ItemDetailModal = ({ item, cart, setCart }) => {
 
 	return (
 		<>
-			<div variant='primary' onClick={handleShow}>
+			<div variant="primary" onClick={handleShow}>
 				<ItemCard item={item} />
 			</div>
 
@@ -48,9 +49,15 @@ const ItemDetailModal = ({ item, cart, setCart }) => {
 					<Modal.Title>{item.name}</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<div style={{ display: 'flex' }}>
-						<div style={{ flexBasis: '30%' }}>Image</div>
-						<div style={{ flexBasis: '70%', display: 'flex', flexDirection: 'column' }}>
+					<div style={{ display: "flex" }}>
+						<div style={{ flexBasis: "30%" }}>Image</div>
+						<div
+							style={{
+								flexBasis: "70%",
+								display: "flex",
+								flexDirection: "column",
+							}}
+						>
 							<div>
 								<b>Select Toping:</b>
 								<Form>
@@ -58,10 +65,27 @@ const ItemDetailModal = ({ item, cart, setCart }) => {
 										<div key={k}>
 											<div>
 												<Form.Check
-													type='checkbox'
-													label={elm.name + ' - $' + elm.price.toFixed(2)}
+													type="checkbox"
+													label={
+														elm.name +
+														" - $" +
+														elm.price.toFixed(2)
+													}
 													id={elm.name}
-													checked={item.addon.includes(elm.name) ? true : false}
+													checked={
+														item.addon.includes(
+															elm.name
+														)
+															? true
+															: false
+													}
+												/>
+												<img
+													src={boba}
+													style={{
+														width: 50,
+														height: 50,
+													}}
 												/>
 											</div>
 										</div>
@@ -71,16 +95,19 @@ const ItemDetailModal = ({ item, cart, setCart }) => {
 							<div>
 								<b>Special Request:</b>
 								<br />
-								<textarea style={{ width: '100%' }}></textarea>
+								<textarea style={{ width: "100%" }}></textarea>
 							</div>
 						</div>
 					</div>
 				</Modal.Body>
 				<Modal.Footer>
-					<Button variant='secondary' onClick={() => setShow(false)}>
+					<Button variant="secondary" onClick={() => setShow(false)}>
 						Cancel
 					</Button>
-					<Button variant='primary' onClick={(e) => addToCartHandler(e)}>
+					<Button
+						variant="primary"
+						onClick={(e) => addToCartHandler(e)}
+					>
 						Add to Cart
 					</Button>
 				</Modal.Footer>
@@ -91,7 +118,9 @@ const ItemDetailModal = ({ item, cart, setCart }) => {
 
 function App() {
 	const [cart, setCart] = useState([]);
-	const [selectedCategory, setSelectedCategory] = useState(categories.milk_tea);
+	const [selectedCategory, setSelectedCategory] = useState(
+		categories.milk_tea
+	);
 	const [itemsToDisplay, setItemsToDisplay] = useState();
 
 	useEffect(() => {
@@ -135,9 +164,9 @@ function App() {
 			<center>
 				<h1>Imaginary Coffee Shop</h1>
 			</center>
-			<div className='App'>
-				<div className='left'>
-					<div className='category'>
+			<div className="App">
+				<div className="left">
+					<div className="category">
 						{Object.entries(categories).map(([key, val]) => (
 							<div key={key} onClick={selectCategoryHandler}>
 								{val}
@@ -145,19 +174,24 @@ function App() {
 						))}
 					</div>
 
-					<div className='item'>
+					<div className="item">
 						{itemsToDisplay?.map((elm, k) => (
-							<ItemDetailModal key={k} item={elm} cart={cart} setCart={setCart} />
+							<ItemDetailModal
+								key={k}
+								item={elm}
+								cart={cart}
+								setCart={setCart}
+							/>
 						))}
 					</div>
 				</div>
 
-				<div className='right'>
+				<div className="right">
 					<center>
 						<h3>Cart</h3>
 					</center>
 					{cart.map((elm, k) => (
-						<div key={k} className='cart-item'>
+						<div key={k} className="cart-item">
 							<div>
 								<div>{elm.name}</div>
 								<ul>
@@ -167,12 +201,24 @@ function App() {
 								</ul>
 							</div>
 							<div>x{elm.qty}</div>
-							<div onClick={(e, item) => SubtractQuantityHandler(e, elm)}>-</div>
-							<div onClick={(e, item) => AddQuantityHandler(e, elm)}>+</div>
+							<div
+								onClick={(e, item) =>
+									SubtractQuantityHandler(e, elm)
+								}
+							>
+								-
+							</div>
+							<div
+								onClick={(e, item) =>
+									AddQuantityHandler(e, elm)
+								}
+							>
+								+
+							</div>
 						</div>
 					))}
 					<center>
-						<Button variant='warning'>Check out</Button>
+						<Button variant="warning">Check out</Button>
 					</center>
 				</div>
 			</div>
--- src/items.js
+++ src/items.js
@@ -1,162 +1,188 @@
 exports.addons = {
-	honey_boba: { name: 'honey boba', price: 0.7, image: '' },
-	crystal_boba: { name: 'crystal boba', price: 0.7, image: '' },
-	egg_pudding: { name: 'egg pudding', price: 0.7, image: '' },
-	taro_pudding: { name: 'taro pudding', price: 0.7, image: '' },
-	aloe_vera: { name: 'aloe vera', price: 0.7, image: '' },
-	grass_jelly: { name: 'grass jelly', price: 0.7, image: '' },
-	rainbow_jelly: { name: 'rainbow jelly', price: 0.7, image: '' },
-	red_bean: { name: 'red bean', price: 0.7, image: '' },
-	cheese_ball: { name: 'cheese ball', price: 1.0, image: '' },
-	cheese_foam: { name: 'cheese foam', price: 1.0, image: '' },
+	oreo: { name: "oreo", price: 0.7, image: "oreo.png" },
+	honey_boba: { name: "honey boba", price: 0.7, image: "honeyboba.png" },
+	crystal_boba: {
+		name: "crystal boba",
+		price: 0.7,
+		image: "crystalboba.png",
+	},
+	egg_pudding: { name: "egg pudding", price: 0.7, image: "pudding.png" },
+	taro_pudding: { name: "taro pudding", price: 0.7, image: "taro.png" },
+	grass_jelly: { name: "grass jelly", price: 0.7, image: "grassjelly.png" },
+	red_bean: { name: "red bean", price: 0.7, image: "redbean.png" },
+	cheese_foam: { name: "cheese foam", price: 1.0, image: "cheesefoam.png" },
+	creme_brulee: {
+		name: "creme brulee",
+		price: 1.0,
+		image: "cremebrulee.png",
+	},
+	brown_sugar: { name: "brown sugar", price: 1.0, image: "brownsugar.png" },
+	lychee_jelly: { name: "lychee jelly", price: 0.7, image: "lychee.png" },
+	boba: { name: "boba", price: 0.7, image: "boba.png" },
 };
 
 exports.categories = {
-	milk_tea: 'milk tea',
-	frostie: 'frostie',
-	yogurt: 'yogurt',
-	munchies: 'munchies',
-	cream_brulee: 'cream brulee',
-	fruit_tea: 'fruit tea',
-	fresh_tea: 'fresh tea',
-	coffee: 'coffee',
+	milk_tea: "milk tea",
+	frostie: "frostie",
+	yogurt: "yogurt",
+	munchies: "munchies",
+	cream_brulee: "cream brulee",
+	fruit_tea: "fruit tea",
+	fresh_tea: "fresh tea",
+	coffee: "coffee",
 };
 
 exports.items = {
-	'01': {
-		name: 'Jasmine Milk Tea',
+	"01": {
+		name: "Jasmine Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'02': {
-		name: 'Thai Milk Tea',
+	"02": {
+		name: "Thai Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'03': {
-		name: 'Honeydew Milk Tea',
+	"03": {
+		name: "Honeydew Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'04': {
-		name: 'Winter Melon Milk Tea',
+	"04": {
+		name: "Winter Melon Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'05': {
-		name: 'Tiger Milk Tea',
+	"05": {
+		name: "Tiger Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!',
-		image: '',
+		description: "This is a sample description, this drink is hella good!!",
+		image: "",
 	},
-	'06': {
-		name: 'Classic Milk Tea',
+	"06": {
+		name: "Classic Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'07': {
-		name: 'Golden Milk Tea',
+	"07": {
+		name: "Golden Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'08': {
-		name: 'Taro Milk Tea',
+	"08": {
+		name: "Taro Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
-	'09': {
-		name: 'Matcha Thai Milk Tea',
+	"09": {
+		name: "Matcha Thai Milk Tea",
 		category: this.categories.milk_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	10: {
-		name: 'Strawberry Frostie',
+		name: "Strawberry Frostie",
 		category: this.categories.frostie,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	11: {
-		name: 'Mango Frostie',
+		name: "Mango Frostie",
 		category: this.categories.frostie,
 		price: 5.0,
-		addon: [this.addons.honey_boba.name, this.addons.cheese_ball.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		addon: [this.addons.honey_boba.name],
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	12: {
-		name: 'Avocado Frostie',
+		name: "Avocado Frostie",
 		category: this.categories.frostie,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	13: {
-		name: 'Taro Frostie',
+		name: "Taro Frostie",
 		category: this.categories.frostie,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	14: {
-		name: 'Lychee Peachee',
+		name: "Lychee Peachee",
 		category: this.categories.fruit_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	15: {
-		name: 'Sunset Beach',
+		name: "Sunset Beach",
 		category: this.categories.fruit_tea,
 		price: 5.0,
 		addon: [this.addons.honey_boba.name],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	16: {
-		name: 'Matcha Coffee',
+		name: "Matcha Coffee",
 		category: this.categories.coffee,
 		price: 5.0,
 		addon: [],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 	17: {
-		name: 'Vietnamese Iced Coffee',
+		name: "Vietnamese Iced Coffee",
 		category: this.categories.coffee,
 		price: 5.0,
 		addon: [],
-		description: 'This is a sample description, this drink is hella good!!!',
-		image: '',
+		description:
+			"This is a sample description, this drink is hella good!!!",
+		image: "",
 	},
 };
